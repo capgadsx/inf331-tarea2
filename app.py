9c306cb7485c0902696d4b19d6aef4cda7f495fe
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2020-11-11 20:47:32 -0300

    Uploading images to S3

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+import boto3
+import logging
 import argparse
 
+global log
+
+def setup_log():
+    global log
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    log = logging.getLogger('inf331-tarea3')
+    log.setLevel(logging.INFO)
+
 def setup_args():
-    parser = argparse.ArgumentParser(description='Compares the text of two images using AWS Rekognition')
+    description_str = """
+    Compares the text of two images using AWS Rekognition\n\n
+    The program needs AWS credentials of an IAM user with
+    AmazonRekognitionFullAccess and AmazonS3FullAccess roles
+    """
+    parser = argparse.ArgumentParser(description=description_str, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-c', '--control', type=str, help='path of the control image', required=True)
     parser.add_argument('-t', '--test', type=str, help='path of the test image', required=True)
     parser.add_argument('-b', '--bucket', type=str, help='bucket to store the images for Rekognition', required=True)
@@ -9,6 +24,30 @@ def setup_args():
     parser.add_argument('-k', '--awskey', type=str, help='AWS secret access key', required=True)
     return parser.parse_args()
 
+def upload_images(args):
+    global log
+    log.info('Creating S3 client, using bucket {}'.format(args.bucket))
+    client = boto3.client(
+        's3',
+        aws_access_key_id=args.awsid,
+        aws_secret_access_key=args.awskey
+    )
+    log.info('Uploading control image: {}'.format(args.control))
+    with open(args.control, 'rb') as fp:
+        client.upload_fileobj(fp, args.bucket, 'control_image')
+    log.info('Uploading test image: {}'.format(args.control))
+    with open(args.test, 'rb') as fp:
+        client.upload_fileobj(fp, args.bucket, 'test_image')
+    log.info('Done uploading images')
+
 
 if __name__ == "__main__":
+    global log
     args = setup_args()
+    setup_log()
+    log.info('Started with args: {}'.format(args))
+    try:
+        upload_images(args)
+    except Exception as excep:
+        log.error('EXCEPTION: {}'.format(excep))
+    log.info('Program done')
